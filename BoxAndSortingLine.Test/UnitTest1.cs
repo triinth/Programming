2af63d4e621cbfa6a390ab72ba2d38717713c01e
Opilane TTHK <opilane@tthk.ee>
2022-09-08 15:42:13 +0300

    xunit test

--- BoxAndSortingLine.Test/UnitTest1.cs
+++ BoxAndSortingLine.Test/UnitTest1.cs
@@ -14,14 +14,27 @@ namespace BoxAndSortingLine.Test
             {
                    new BoxSize
             {
-                Length = 200,
-                Width = 87,
+                Length = 70,
+                Width = 50,
                 SortingLineParams = new List<SortingLineParam>
                 {
                     new SortingLineParam
                     {
-                        LineWidth = 75
-                    }
+                        LineWidth = 50,
+                    },
+                     new SortingLineParam
+                    {
+                        LineWidth = 50,
+                    },
+                      new SortingLineParam
+                    {
+                        LineWidth = 100,
+                    },
+                       new SortingLineParam
+                    {
+                        LineWidth = 50,
+                    },
+
                 }
             }
             };
